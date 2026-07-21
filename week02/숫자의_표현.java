7ff174c7b86aabd3146f533adc928d2c83016914
Novice-Dev-Robin <dev.robin03@gmail.com>
2026-07-21 16:21:35 +0900

    2주차 예제 완료

--- week02/숫자의_표현.java
+++ week02/숫자의_표현.java
@@ -1,5 +1,28 @@
 package week02;
 
-public class 숫자의_표현 {
-    
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        int total = 0; // 총합 중간 계산용
+        
+        for(int cnt = 1; cnt < n; cnt++) {
+            
+            total = 0; // 총합 초기화
+            
+            for(int cnt2 = cnt; cnt2 < n; cnt2++) {
+                total += cnt2;
+                
+                if(total == n) {
+                    answer++;
+                    break;
+                }
+                
+                else if(total > n) break;
+            }
+        }
+        
+        answer += 1; // 자기 자신 포함
+        return answer;
+    }
 }
--- week02/최댓값_만들기(1).java
+++ week02/최댓값_만들기(1).java
@@ -1,5 +1,8 @@
 package week02;
 
+// 정수 배열 numbers가 매개변수로 주어집니다.
+// numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을 return하도록 solution 함수를 완성해주세요.
+
 import java.util.*;
 
 class Solution {
@@ -11,7 +14,7 @@ class Solution {
         
         // Integer 타입 : 래퍼클래스
         // 따라서, Collections 사용 가능
-        Arrays.sort(newArray,Collections.reverseOrder());
+        Arrays.sort(newArray, Collections.reverseOrder());
         answer = newArray[0] * newArray[1];
         
         return answer;
